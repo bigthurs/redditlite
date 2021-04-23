626360f883e2a1f0e5f987dc37a2ede72bf5549f
Trevor Hursey <trevorhursey@yahoo.com>
2021-04-23 13:16:48 -0700

    cleaned up return shape of errors, added email to user object

---
+++ server/src/utils/sendEmail.ts
@@ -0,0 +1,26 @@
+import nodemailer from "nodemailer";
+
+export async function sendEmail(to: string, html: string) {
+  //   let testAccount = await nodemailer.createTestAccount();
+  //   console.log(testAccount);
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "ynxf5ae7fgj3uv6e@ethereal.email", // generated ethereal user
+      pass: "yZ9D1aTXc7ZYYnE4hU", // generated ethereal password
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: to, // list of receivers
+    subject: "Change password", // Subject line
+    html,
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,10 +1,7 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
-import { withUrqlClient } from "next-urql";
 import theme from "../theme";
-import { Provider } from "urql";
-import { createUrqlClient } from "../utils/createUrqlClient";
 
-function MyApp({ Component, pageProps, urqlClient }: any) {
+function MyApp({ Component, pageProps }: any) {
   return (
     <ChakraProvider resetCSS theme={theme}>
       <ColorModeProvider
